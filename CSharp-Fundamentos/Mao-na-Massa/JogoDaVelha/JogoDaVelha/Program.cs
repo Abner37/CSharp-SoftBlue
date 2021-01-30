df2125fc6fb860ba7eb50327e78aa2eea34a046a
Abner <Abner37@gmail.com>
2021-01-30 16:14:57 -0300

    Added JogoDaVelha project

---
+++ CSharp-Fundamentos/Mao-na-Massa/JogoDaVelha/JogoDaVelha/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JogoDaVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancia o jogo e inicia.
+            Game game = new Game();
+            game.Play();
+        }
+    }
+}
